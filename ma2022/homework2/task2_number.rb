e3cdfa6f8848276fec927e54e3dcdb6f0874725b
oksana.skalko <skalkomargo@gmail.com>
2021-11-08 12:19:48 +0200

    add method count to the task2

--- ma2022/homework2/task2_number.rb
+++ ma2022/homework2/task2_number.rb
@@ -1,8 +1,8 @@
 loop do |variable|
 	print 'Enter the number: '
 	number = gets.to_i
-	check_number = number.to_s.split('').map(&:to_i).select { |int| int == 1}
-	if check_number.size > 1
+	check_number = number.to_s.split('').map(&:to_i)
+	if check_number.count(1) > 1
 		puts "Conditions are not met, I stop the program."
 		break
 	end
