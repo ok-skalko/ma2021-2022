09b530c1c5a654bfbcd34f3f456020e1adced1b9
oksana.skalko <skalkomargo@gmail.com>
2021-11-10 15:35:21 +0200

    fix task1, task2, task4

--- ma2022/homework2/task2_number.rb
+++ ma2022/homework2/task2_number.rb
@@ -1,9 +1,10 @@
 loop do |variable|
 	print 'Enter the number: '
-	number = gets
-	if number.split('').map(&:to_i).count(1) > 1
+	number = gets.chomp
+	if number.count('1') <= 1 && number == "#{number.to_i}"
+		puts "Execution result: #{number.to_i * 256}"
+	else
 		puts "Conditions are not met, I stop the program."
 		break
 	end
-	puts "Execution result: #{number.to_i * 256}"
 end
--- ma2022/homework2/task4.rb
+++ ma2022/homework2/task4.rb
@@ -1,20 +1,22 @@
 arr = %w[a b c d] #це "рубішна" скорочена форма запису ось цього - ['a', 'b', 'c', 'd']
 
 def arr.my_own_map
-  collection_counter = 0
-  result = []
-  until collection_counter == size
-    element = yield(self[collection_counter])
-    if element.is_a? Integer
-      result << element * 2
-    else
-      result << element
+  if block_given?
+    collection_counter = 0
+    result = []
+    until collection_counter == size
+      element = yield(self[collection_counter])
+      if element.is_a? Integer
+        result << element * 2
+      else
+        result << element
+      end
+      collection_counter += 1
     end
-    collection_counter += 1
+    result
+  else
+    puts 'Please add a block'
   end
-  result
-  rescue LocalJumpError
-  puts 'Please add a block'
 end
 
 # відмінність - метод my_own_map можна викликати тільки на масиві, записаному в змінну arr
